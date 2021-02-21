b94aa806f1a1ec6a5def7c67468639386dc11813
melogo5 <i.plyton@gmail.com>
2021-02-22 04:49:33 +0500

    feat: add min, max, avg functions solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array) return 0;
+  if (!array.length) return 0;
+  let minValue = array[0];
+  array.map(function(elem) {
+    if(elem < minValue) minValue = elem;
+  });
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array) return 0;
+  if (!array.length) return 0;
+  let maxValue = array[0];
+  array.map(function(elem) {
+    if(elem > maxValue) maxValue = elem;
+  });
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array) return 0;
+  if (!array.length) return 0;
+  let sum = 0;
+  array.map(function(elem) {sum += elem})
+  return sum / array.length;
 }
